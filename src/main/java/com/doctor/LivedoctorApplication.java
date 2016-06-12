14cc00e45cc97c683883548d184f6801c79fb29c
NistorNicu <nistor.nicu91@gmail.com>
2016-06-12 19:49:14 +0300

    push sources

---
+++ src/main/java/com/doctor/LivedoctorApplication.java
@@ -0,0 +1,12 @@
+package com.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivedoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivedoctorApplication.class, args);
+	}
+}
